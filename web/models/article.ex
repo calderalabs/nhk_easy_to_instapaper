ccabcbced7a3a5cd9fb0dcfa4d37437085537bf4
Matteo Depalo <matteodepalo@gmail.com>
2017-01-16 09:08:26 +0000

    Remove `a` tags

--- web/models/article.ex
+++ web/models/article.ex
@@ -8,19 +8,33 @@ defmodule NhkEasyToInstapaper.Article do
   def body(article) do
     HTTPoison.get!(url(article)).body
     |> Floki.find("#newsarticle")
-    |> remove_rt_tags
+    |> remove_rt_tags_and_content
+    |> remove_a_tags
     |> Floki.raw_html
     |> HtmlSanitizeEx.basic_html
   end
 
-  defp remove_rt_tags(html) do
+  defp remove_rt_tags_and_content(html) do
     step = fn(tag, attrs, children) ->
-      {tag, attrs, children |> Enum.map(&(remove_rt_tags(&1))) |> Enum.reject(&(&1 == nil))}
+      {tag, attrs, children |> Enum.map(&(remove_rt_tags_and_content(&1))) |> Enum.reject(&(&1 == nil))}
     end
 
     case html do
       [{tag, attrs, children}] -> [step.(tag, attrs, children)]
       {"rt", _, _} -> nil
+      {tag, attrs, children} -> step.(tag, attrs, children)
+      x -> x
+    end
+  end
+
+  defp remove_a_tags(html) do
+    step = fn(tag, attrs, children) ->
+      {tag, attrs, children |> Enum.map(&(remove_a_tags(&1)))}
+    end
+
+    case html do
+      [{tag, attrs, children}] -> [step.(tag, attrs, children)]
+      {"a", _, [children]} -> children
       {tag, attrs, children} -> step.(tag, attrs, children)
       x -> x
     end
